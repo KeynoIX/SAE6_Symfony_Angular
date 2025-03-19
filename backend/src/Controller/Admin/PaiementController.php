47de2c66f34d55d4b170f9db47fc81ab31cd5adb
tristandlth <85364501+tristandlth@users.noreply.github.com>
2025-03-19 15:09:53 +0100

    Fiche de paies

--- backend/src/Controller/Admin/PaiementController.php
+++ backend/src/Controller/Admin/PaiementController.php
@@ -5,6 +5,8 @@ namespace App\Controller\Admin;
 use App\Entity\Coach;
 use App\Entity\Seance;
 use Doctrine\ORM\EntityManagerInterface;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,6 +29,7 @@ class PaiementController extends AbstractController
             $total = $seancesValidees * $coach->getTarifHoraire();
 
             $paiements[] = [
+                'id' => $coach->getId(), // Ajout de l'ID
                 'coach' => $coach->getPrenom() . ' ' . $coach->getNom(),
                 'seances_validees' => $seancesValidees,
                 'tarif_horaire' => $coach->getTarifHoraire(),
@@ -37,5 +40,48 @@ class PaiementController extends AbstractController
         return $this->render('admin/paiements.html.twig', [
             'paiements' => $paiements
         ]);
+    }
+
+    #[Route('/paiements/pdf/{id}/{periode}', name: 'paiement_pdf')]
+    public function generatePdf(EntityManagerInterface $entityManager, int $id, string $periode): Response
+    {
+        $coach = $entityManager->getRepository(Coach::class)->find($id);
+
+        if (!$coach) {
+            throw $this->createNotFoundException('Coach introuvable.');
+        }
+
+        $seances = $entityManager->getRepository(Seance::class)->findBy([
+            'coach_id' => $coach,
+            'statut' => 'Validée'
+        ]);
+
+        $totalHeures = count($seances);
+        $montantTotal = $totalHeures * $coach->getTarifHoraire();
+
+        $html = $this->renderView('admin/paiement_pdf.html.twig', [
+            'coach' => $coach,
+            'periode' => $periode,
+            'totalHeures' => $totalHeures,
+            'tarifHoraire' => $coach->getTarifHoraire(),
+            'montantTotal' => $montantTotal
+        ]);
+
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        $dompdf = new Dompdf($pdfOptions);
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        return new Response(
+            $dompdf->output(),
+            200,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'attachment; filename="Fiche de paie de ' . $coach->getNom() . ' ' . $coach->getPrenom() . ' ' . $periode . '.pdf"'
+            ]
+        );
     }
 }
